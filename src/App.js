7fc9a4523878bc04ea9fb46736baf2d0b4829f01
Rebekah Milford <rmilford86@gmail.com>
2016-08-03 11:22:22 -0400

    hello world

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
-// Developer TODO: Define App component defintion here
+import React, {Component} from 'react'
+
+// define our Hello component
+class Hello extends Component {
+  // what should the component render
+  render () {
+    // Make sure to return some UI
+    return (
+      <h1>Hello World!</h1>
+        <button type="button">Click Me!</button>
+    )
+  }
+}
+
+export default Hello
